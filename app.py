0ecb1efde620301c15d9478ca0e6d1016f50fcf8
hariradhautpala <hdoradla2023@fau.edu>
2025-01-31 20:04:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as stst.image("https://v0-horoscope-isxbvbudo-hari-radha-utpala-doradlas-projects.vercel.app", caption="AI-Powered Streamlit App") 
 import requests
 import json
 import base64
